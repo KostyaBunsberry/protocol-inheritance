43502ab8da20e5538bf3d986c8f2194af744a21d
Kostya Bunsberry <kostya.bunsberry@gmail.com>
2023-09-08 03:43:32 +0700

    Initial Commit

---
+++ protocol-inheritance/protocol_inheritanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  protocol_inheritanceApp.swift
+//  protocol-inheritance
+//
+//  Created by Kostya Bunsberry on 08.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct protocol_inheritanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
